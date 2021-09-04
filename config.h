66c466a7439b0bdf228915060be174a307e3b189
SCUTOIDA <67786490+scutoidh@users.noreply.github.com>
2021-09-04 18:22:45 +0800

    Update config.h

--- config.h
+++ config.h
@@ -75,6 +75,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *print_screen_cmd[] = { "scrot", "%Y-%m-%d-%H%M%S.png", "-e", "mv $f ~/images/screenshots", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -82,6 +83,7 @@ static Key keys[] = {
 	{ 0,                       XF86XK_AudioRaiseVolume,      spawn,          {.v = downvol } },
 	{ 0,                       XF86XK_AudioMute,      spawn,          {.v = mutevol } },
 	{ 0,                       XF86XK_AudioMicMute,      spawn,          {.v = mutemic } },
+	{ 0,                            XK_Print,   spawn,          {.v = print_screen_cmd } },
 	
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
