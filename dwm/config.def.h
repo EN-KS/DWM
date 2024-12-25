2eb54d6509a655729007db71e35c16fd9d99e30b
Redbeard93 <67786490+Redbeard93@users.noreply.github.com>
2024-12-26 02:19:00 +0800

    Update config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -8,12 +8,18 @@ static const char *mutemic[] = {"amixer",  "-qM",    "sset",
 
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
-static const int startwithgaps[] = {1};
-static const unsigned int gappx[] = {1};
-static const unsigned int snap = 32; /* snap pixel */
-static const int showbar = 1;        /* 0 means no bar */
-static const int topbar = 1;         /* 0 means bottom bar */
+static const unsigned int snap = 32;    /* snap pixel */
+static const int showbar = 1;           /* 0 means no bar */
+static const int topbar = 1;            /* 0 means bottom bar */
 static const char *fonts[] = {"Sarasa Term SC Nerd:size=16"};
+static const unsigned int systraypinning =
+    0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
+          X */
+static const unsigned int systrayspacing = 2; /* systray spacing */
+static const int systraypinningfailfirst =
+    1; /* 1: if pinning fails, display systray on the first monitor, False:
+          display systray on the last monitor*/
+static const int showsystray = 1; /* 0 means no systray */
 static const char dmenufont[] = "Sarasa Term SC Nerd:size=10";
 static const char col_gray1[] = "#100D23";
 static const char col_gray2[] = "#9E9E61";
@@ -52,6 +58,7 @@ static const float mfact = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1;    /* number of clients in master area */
 static const int resizehints =
     1; /* 1 means respect size hints in tiled resizals */
+static const int decorhints = 1; /* 1 means respect decoration hints */
 static const int lockfullscreen =
     1; /* 1 will force focus on the fullscreen window */
 
@@ -83,75 +90,7 @@ static const char *dmenucmd[] = {
     "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
     "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
 static const char *termcmd[] = {"kitty", NULL};
-static const char *chromium[] = {"chromium", NULL};/* See LICENSE file for copyright and license details. */
-    { 0,                      0x1008ff41,      spawn,          {.v = chromium } },/* 将ThinkVantage键绑定为chromium浏览器，0x1008ff41为16进制keysym*/
-    { 0,         XF86XK_AudioLowerVolume,      spawn,          {.v = downvolume } },
-    { 0,         XF86XK_AudioRaiseVolume,      spawn,          {.v = upvolume } },
-    { 0,                XF86XK_AudioMute,      spawn,          {.v = mutevolume } },
-    { 0,             XF86XK_AudioMicMute,      spawn,          {.v = mutemic } },
-    { 0,                        XK_Print,      spawn,          {.v = print_screen_cmd } },
-    { 0,          XF86XK_MonBrightnessUp,      spawn,          {.v = brightnessup } },/* 系统默认增加亮度的KeySym*/
-    { 0,        XF86XK_MonBrightnessDown,      spawn,          {.v = brightnessdown } },/* 系统默认降低亮度的KeySym*/
-	{ MODKEY|ShiftMask,             XK_e, spawn,          {.v = powermenu } },
-	{ MODKEY|ShiftMask,             XK_n, spawn,          {.v = wifi_menu } },
-	{ MODKEY|ShiftMask,             XK_t, spawn,          {.v = htop } },
-	{ MODKEY,                       XK_p,      spawn,          {.v = rofi } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-        { MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
-	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = GAP_RESET } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-};
-
-/* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
-static const Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
-};
-
-
+static const char *chromium[] = {"chromium", NULL};
 static const char *print_screen_cmd[] = {"scrot", "%Y-%m-%d-%H%M%S.png", "-e",
                                          "mv $f ~/Pictures/screenshots", NULL};
 static const char *brightnessup[] = {
