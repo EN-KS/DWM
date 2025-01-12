e3e34dc0c12142d7fba1d0191d02c82354dc9896
Redbeard93 <67786490+Redbeard93@users.noreply.github.com>
2025-01-12 13:30:25 +0800

    Update config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -41,7 +41,7 @@ static const unsigned int alphas[][3] = {
 };
 /* tagging */
 static const char *tags[] = {
-    "☰", "☱", "☲", "☳", "☴", "☵", "☶", "☷", "", "󰘑",
+    "☰", "☱", "☲", "☳", "☴", "☵", "☶", "☷", "󰚀",
 };
 
 static const Rule rules[] = {
