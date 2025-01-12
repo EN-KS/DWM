58e303e624a113dc2802716529d1ed00c9e832a3
Redbeard93 <67786490+Redbeard93@users.noreply.github.com>
2025-01-12 16:30:13 +0800

    Update config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -11,7 +11,7 @@ static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"WenQuanYi Zen Hei:size=16","NotoSansM NFM:size=16",};
+static const char *fonts[] = {"WenQuanYi Zen Hei:size=16",};
 static const unsigned int systraypinning =
     0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
           X */
